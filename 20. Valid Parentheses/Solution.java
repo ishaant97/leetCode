ad2def2ab2fe90f8215c9ea47155b9bd71a8eac2
Ishaant Kumar Singh <ishaant97@gmail.com>
2023-08-19 20:24:08 +0530

    20. Valid Parentheses Done

---
+++ 20. Valid Parentheses/Solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
