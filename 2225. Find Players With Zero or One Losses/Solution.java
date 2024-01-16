fc7d8610ed2b23126df35d7e8e7b6212aa7d2595
Ishaant Kumar Singh <ishaant97@gmail.com>
2024-01-16 13:35:31 +0530

    2225. Find Players With Zero or One Losses

---
+++ 2225. Find Players With Zero or One Losses/Solution.java
@@ -0,0 +1,42 @@
+// not my solution but I understood it
+
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> findWinners(int[][] matches) {
+        int[] losses = new int[100001];
+
+        for (int i = 0; i < matches.length; i++) {
+            int win = matches[i][0];
+            int loss = matches[i][1];
+
+            if (losses[win] == 0) {
+                losses[win] = -1;
+            }
+
+            if (losses[loss] == -1) {
+                losses[loss] = 1;
+            } else {
+                losses[loss]++;
+            }
+        }
+
+        List<Integer> zeroLoss = new ArrayList<>();
+        List<Integer> oneLoss = new ArrayList<>();
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < losses.length; i++) {
+            if (losses[i] == -1) {
+                zeroLoss.add(i);
+            } else if (losses[i] == 1) {
+                oneLoss.add(i);
+            }
+        }
+
+        result.add(zeroLoss);
+        result.add(oneLoss);
+
+        return result;
+    }
+}
